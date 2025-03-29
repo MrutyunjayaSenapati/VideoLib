d040faa5b6e53b07c9df30b9042609f023d1d98e
Mrutyunjaya Senapati <mrutyunjayasenapati007@gmail.com>
2025-03-30 02:08:37 +0530

    CR

--- Backend/src/routes/adminRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const adminController = require('../controllers/adminController.js');
-
-const dbMiddleware = require('../middleware/dbMiddleware.js');
-router.use(dbMiddleware);
-router.post('/login', adminController.loginAdmin);
-module.exports = router;
